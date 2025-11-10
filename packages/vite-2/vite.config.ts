d2bb248e8f80bfe4ade991ae278cffb7f64bad92
Sutin <poly.dev2018@gmail.com>
2025-11-10 21:17:35 +0700

    chore: ignore dist and pnpm-lock.yaml in all projects

--- packages/vite-2/vite.config.ts
+++ packages/vite-2/vite.config.ts
@@ -1,9 +1,19 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+  viteStaticCopy({
+    targets: [
+      {
+        src: 'src/assets/iframe-x.html',
+        dest: 'iframe-x.html'
+      }
+    ]
+  })
+  ],
   server: {
     port: 5174
   }
